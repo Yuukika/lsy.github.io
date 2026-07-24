e930ed147c4747abf1616ec357186123948851b7
Luwenbin <758774239@qq.com>
2026-07-24 13:01:26 +0800

    Add files via upload

---
+++ template/baisu/list.php
@@ -0,0 +1,19 @@
+<?php
+
+// 主题开发文档：https://doc.lylme.com/dev/theme
+$rel = $conf["mode"] == 2 ? '' : 'rel="nofollow"';
+	$link_desc = '';
+		if (theme_config('link_desc') == '1') {
+			$link_desc = '{link_desc}';
+		}
+
+$html = array(
+    'g1' => '<div class="site-name" id="category-{group_id}">{group_icon}{group_name}</div>', //分组开始标签
+    'g2' => '<div class="site-list">',  //分组内容
+    'g3' => '<div class="list kongs"></div><div class="list kongs"></div><div class="list kongs"></div><div class="list kongs"></div></div>',  //分组结束标签
+
+    'l1' => '<div class="list urllist" id="id_{link_id}" data-id="{link_id}" data-url="{link_url}">',  //链接开始标签
+    'l2' => '<a '.$rel.' href="{link_url}" title="{link_name_text}"  target="_blank">{link_icon}',  //链接内容
+    'l3' => '<p class="name">{link_name}</p></a><p class="desc">'.$link_desc.'</p></div>',  //链接结束标签
+);
+lists($html);
